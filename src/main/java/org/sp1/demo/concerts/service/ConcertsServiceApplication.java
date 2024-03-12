c55dbd2ec2f8061be86ab7fe605fc075a057740d
tongdayong <bryanxtong@gmail.com>
2024-03-12 14:17:40 +0800

    update to spring boot 3.2.3

--- src/main/java/org/sp1/demo/concerts/service/ConcertsServiceApplication.java
+++ src/main/java/org/sp1/demo/concerts/service/ConcertsServiceApplication.java
@@ -3,10 +3,13 @@ package org.sp1.demo.concerts.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -18,13 +21,17 @@ public class ConcertsServiceApplication {
     }
 
     @Configuration
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()
-                    .and().csrf().disable();
+    @EnableWebSecurity
+    public static class SecurityPermitAllConfig {
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.authorizeHttpRequests(authorizeHttpRequests ->
+                            authorizeHttpRequests
+                                    .anyRequest()
+                                    .permitAll())
+                    .csrf(AbstractHttpConfigurer::disable);
+            return http.build();
         }
     }
-
 
 }
